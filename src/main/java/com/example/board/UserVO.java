9a8d21e8e4f8d14ba48eab593a0934e8b39a451e
mingseobi <mingseobi@naver.com>
2023-12-16 18:00:33 +0900

    final_commit

--- src/main/java/com/example/board/UserVO.java
+++ src/main/java/com/example/board/UserVO.java
@@ -8,23 +8,18 @@ public class UserVO {
     public String getUserid() {
         return userid;
     }
-
     public void setUserid(String userid) {
         this.userid = userid;
     }
-
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,15 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+
+</head>
+<body>
+<form method="post" action="loginOk" class="login-form">
+    <div><label>User ID :</label><input type="text" name="userid"></div>
+    <div><label>User password :</label>
+        <input type="password" name="password"></div>
+    <button type="submit">Log-in</button>
+</form>
+</body>
+</html>
